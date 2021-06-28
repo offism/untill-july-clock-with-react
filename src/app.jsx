8ec49405ba3822600019b392f71f021498f8b73b
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-06-28 22:54:23 +0300

    add repo files

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,32 @@
 import {useState} from 'react'
 import './App.scss'
+import {BrowserRouter , Route , Switch ,Link} from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
 
 export default function App (){
-	const [students , setStudents] = useState([])
+	return (
 
-	const handleKeyUp = e => { 
-		if(e.keyCode === 13){
-			setStudents([...students , e.target.value])
-			e.target.value = null
-		}
-	}
+	<BrowserRouter>
+     
+       <nav>
+       	 <ul>
+       	 	<li>
+       	 		<Link to="/">Home</Link>
+       	 	</li>
+       	 	<li>
+       	 		<Link to="/about">About</Link>
+       	 	</li>
+       	 </ul>
+       </nav>
 
-return(
-	<>
-	 <h1>Hello world</h1>
-	 <input type="text" onKeyUp={handleKeyUp} />
-	 <ul>
-       {
-       	students.map((student , index) =>{
-         return (
-       	<li key={index}>{student}</li>
-         	)
-       	})
-       }
-	 </ul>	
-	</ >
-	)
+	  <Switch>
+	  	 <Route path="/" exact component={Home} />
 
+	  	 <Route path="/about" exact component={About} />
+	  	 	
+	  </Switch>
+	</BrowserRouter>
+
+		)
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+export default function Home(){
+	return(
+		<>
+        <h1>Home</h1>
+
+        <main className="main">
+        	<h1 className="main__title">Coming soon!</h1>
+        	<div className="countDown">
+              <span className="day">01</span>
+              <span className="hour">14</span>
+              <span className="minute">04</span>
+              <span className="second">20</span>
+        	</div>				
+        </main>
+        </ >
+		)
+}
